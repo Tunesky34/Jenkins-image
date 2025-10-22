82898e05ebed3a322cf3f66243101e90ec0f3cfc
Devopscohort1 <dwtdevopscohort1@gmail.com>
2025-10-22 18:37:03 -0500

    Add index.html and main.jsx for Vite build

---
+++ frontend/src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <h1>✅ Jenkins Vite Build Successful!</h1>;
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
